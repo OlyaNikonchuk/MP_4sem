7ae38cd2ab85762f5f2af67920cca5c05d7b3041
Ollie <70698710+OlyaNikonchuk@users.noreply.github.com>
2022-04-06 16:10:25 +0300

    Add files via upload

---
+++ Lab_01/Lab_01/Auxil.h
@@ -0,0 +1,10 @@
+#pragma once
+#pragma once
+#include <cstdlib>
+
+namespace auxil
+{
+	void start(); // старт генерации случвйного числа
+	double dget(double dmin, double dmax); //получить случайное число
+	int iget(int imin, int imax); //получить случайное число
+}
